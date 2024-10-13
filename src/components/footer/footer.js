c4c2dcf4ecbc1c2666ffd439ccdc14ea1d92399e
이해준 <kyo2263@naver.com>
2024-10-13 14:54:09 +0900

    foo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -83,57 +83,169 @@ const footerSections = [
 
 const Footer = () => {
   return (
-    <div className="flex flex-col gap-m w-full">
-      {footerSections.map((section, index) => (
-        <div
-          key={index}
-          className="first:mt-0 mt-m m:mt-0 flex flex-col gap-m w-full"
-        >
-          <div className="gap-3xs flex flex-col">
-            <span
-              className="text-footer-label text-small"
-              role="heading"
-              aria-level="2"
-            >
-              {section.title}
-            </span>
-            <ul className="gap-3xs flex flex-col" role="list">
-              {section.links.map((link, linkIndex) => (
-                <li key={linkIndex} role="listitem">
-                  <a
-                    className="transition ease-curve-a duration-250 p-0.5 -m-0.5 text-small text-footer-link"
-                    href={link.href}
-                    target={link.external ? "_blank" : "_self"}
-                    rel={link.external ? "noopener noreferrer" : undefined}
-                  >
-                    {link.text}
-                    {link.external && (
-                      <svg
-                        width="0.5625rem"
-                        viewBox="0 0 10 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1 9L9 1M9 1H2.5M9 1V7.22222"
-                          stroke="currentColor"
-                          strokeWidth="1.25"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>
-                      </svg>
-                    )}
-                    {link.external && (
-                      <span className="sr-only">(opens in a new window)</span>
-                    )}
-                  </a>
-                </li>
-              ))}
-            </ul>
+    <>
+      <div className="flex justify-center items-center">
+        {/* 큰 전체 */}
+        <div className="max-w-[68%] mb-[10%] bg-red-500 h-full ">
+          {/* 컨텐츠 전체 */}
+          <div className="flex gap-16">
+            {/* 첫번째 줄 */}
+            <div className="flex flex-col gap-16">
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Our research</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>Overview</a>
+                  </li>
+                  <li>
+                    <a>Index</a>
+                  </li>
+                </ul>
+              </div>
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Latest advancements</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>OpenAI o1</a>
+                  </li>
+                  <li>
+                    <a>OpenAI o1-mini</a>
+                  </li>
+                  <li>
+                    <a>GPT-4</a>
+                  </li>
+                  <li>
+                    <a>GPT-4o mini</a>
+                  </li>
+                  <li>
+                    <a>DALL·E 3</a>
+                  </li>
+                  <li>
+                    <a>Sora</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* 두번째 줄 */}
+            <div className="flex flex-col gap-16">
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>ChatGPT</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>For Everyone</a>
+                  </li>
+                  <li>
+                    <a>For Teams</a>
+                  </li>
+                  <li>
+                    <a>For Enterprises</a>
+                  </li>
+                  <li>
+                    <a>ChatGPT login</a>
+                  </li>
+                  <li>
+                    <a>Download</a>
+                  </li>
+                </ul>
+              </div>
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>API</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>Platform overview</a>
+                  </li>
+                  <li>
+                    <a>Pricing</a>
+                  </li>
+                  <li>
+                    <a>Documentation</a>
+                  </li>
+                  <li>
+                    <a>API login</a>
+                  </li>
+                </ul>
+              </div>
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Explore more</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>OpenAI for business</a>
+                  </li>
+                  <li>
+                    <a>Stories </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* 세번째 줄 */}
+            <div className="flex flex-col gap-16">
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Safety overview</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>Safety overview</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* 네번째 줄 */}
+            <div className="flex flex-col gap-16">
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Company</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>About us</a>
+                  </li>
+                  <li>
+                    <a>News</a>
+                  </li>
+                  <li>
+                    <a>Our Charter</a>
+                  </li>
+                  <li>
+                    <a>Security</a>
+                  </li>
+                  <li>
+                    <a>Residency</a>
+                  </li>
+                  <li>
+                    <a>Careers</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* 다섯번째 줄 */}
+            <div className="flex flex-col gap-16">
+              {/* 소 타이틀 */}
+              <div className="flex flex-col gap-4">
+                <span>Terms & policies</span>
+                <ul className="flex flex-col">
+                  <li>
+                    <a>Terms of use</a>
+                  </li>
+                  <li>
+                    <a>Privacy policy</a>
+                  </li>
+                  <li>
+                    <a>Brand guidelines</a>
+                  </li>
+                  <li>
+                    <a>Other policies</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
